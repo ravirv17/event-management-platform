22134d9579d538054f3798f4f3839f9b95c6030f
ravirv17 <ravirv1717@gmail.com>
2025-02-08 02:20:38 +0530

    made changes to server.js

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,8 @@ io.on("connection", (socket) => {
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    server.listen(5000, () => console.log("🚀 Server running on port 5000"));
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
